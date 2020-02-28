114fa8320efab34389082093dd5b8031dbfacfa5
Artem Bondarenko <artemb@kth.se>
2020-02-28 15:59:55 +0000

    added movement of interpolation accelerators between engine instances

--- util/cuda-backend.h
+++ util/cuda-backend.h
@@ -57,9 +57,9 @@ class cuFFTEngine: public FFTEngine<float>
 
         //move operations are allowed tho
         cuFFTEngine(cuFFTEngine&& ref)
-        { std::swap(plan, ref.plan); std::swap(data, ref.data); }
+        { std::swap(plan, ref.plan); std::swap(data, ref.data); std::swap(InterpAccel, ref.InterpAccel); }
         cuFFTEngine& operator=(cuFFTEngine&& ref)
-        { std::swap(plan, ref.plan); std::swap(data, ref.data); return *this; }
+        { std::swap(plan, ref.plan); std::swap(data, ref.data); std::swap(InterpAccel, ref.InterpAccel); return *this; }
 
         std::string Init( std::size_t, std::size_t maxBatch = 0, std::size_t maxMem = 0 );
         bool RunTransform( float*, float norm = 1.0f, std::size_t padding = 0 );
