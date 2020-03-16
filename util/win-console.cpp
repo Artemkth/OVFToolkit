4a64fa28c8ca150af6534e61c7d6c573c3448559
Artem Bondarenko <artemb@kth.se>
2020-03-16 17:18:14 +0000

    Implemented windows console interfaces

---
+++ util/win-console.cpp
@@ -0,0 +1,28 @@
+#include"console.h"
+#include<io.h>
+#include<stdio.h>
+#include<Windows.h>
+
+struct ConsoleInfo::ConsoleMetadata {
+	HANDLE hTerminal{ INVALID_HANDLE_VALUE };
+
+	ConsoleMetadata() : hTerminal(GetStdHandle(STD_OUTPUT_HANDLE)) {};
+};
+
+ConsoleInfo::ConsoleInfo() noexcept : meta(new ConsoleInfo::ConsoleMetadata()), isRedirected(_isatty(_fileno(stdout))) {}
+
+ConsoleInfo::~ConsoleInfo() noexcept
+{
+	delete meta;
+}
+
+int ConsoleInfo::GetConsoleWidth() const
+{
+	CONSOLE_SCREEN_BUFFER_INFO cInfo;
+
+	if (meta->hTerminal == INVALID_HANDLE_VALUE || !GetConsoleScreenBufferInfo(meta->hTerminal, &cInfo))
+		return 0;
+
+	return cInfo.dwMaximumWindowSize.X;
+}
+
